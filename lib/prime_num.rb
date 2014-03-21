2d861d672ae916cb5b0c11898e4372168d402003
mikekauffman <mike.a.kauffman@gmail.com>
2014-03-21 09:29:26 -0600

    refactored the spec file

--- lib/prime_num.rb
+++ lib/prime_num.rb
@@ -5,3 +5,4 @@ class PrimeNum
     Prime.prime?(number)
   end
 end
+
--- spec/prime_num_spec.rb
+++ spec/prime_num_spec.rb
@@ -4,14 +4,10 @@ require 'prime_num'
 describe PrimeNum do
   it "determines if a number is not a prime number" do
     number = PrimeNum.new
-    expected = false
-    actual = number.prime_number?(85)
-    expect(actual).to eq expected
+    expect(number.prime_number?(85)).to eq false
   end
   it "determines if a number is a prime number" do
     number = PrimeNum.new
-    expected = true
-    actual = number.prime_number?(17)
-    expect(actual).to eq expected
+    expect(number.prime_number?(17)).to eq true
   end
 end
